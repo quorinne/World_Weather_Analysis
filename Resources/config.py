fdad5f1529022d0bd9f5e030a8ee471c6a52641a
Brittanie <brittaniejones@ymail.com>
2022-03-11 12:18:57 -0800

    WeatherPy

---
+++ Resources/config.py
@@ -0,0 +1,2 @@
+# API KEY
+weather_api_key="ba457b34dacf6b1c09ef8dade0e8f305"
---
+++ config.py
@@ -0,0 +1,5 @@
+# API KEY
+weather_api_key="ba457b34dacf6b1c09ef8dade0e8f305"
+NYT_API_KEY ="zQti5QaPpoOmOvbJqyYhCPrBl3AAfovo"
+    # Google
+Google_API_KEY = "AIzaSyBYF7Nd0fTQNrAqiXmkeGJCq8sMzhI3sfM"
